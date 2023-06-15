51ccfcee04318850d61e6074429a5684aec3a160
aborzym <borzym.a@gmail.com>
2023-06-15 12:48:53 +0200

    all done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,5 @@
 "use strict";
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const gallery = document.querySelector(".gallery");
 const imagesArray = galleryItems.map(
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+"use strict";
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const imagesArray = galleryItems.map(
+  (img) =>
+    `<li class="gallery__li">
+        <a class="gallery__item" href="${img.original}">
+            <img
+                class="gallery__image"
+                src="${img.preview}" 
+                alt="${img.description}"
+            />
+        </a>
+     </li>`
+);
+
+gallery.insertAdjacentHTML("beforeend", imagesArray.join(""));
+
+gallery.addEventListener("click", showBigImage);
+
+function showBigImage(event) {
+  //zablokować domyślne zachowanie
+  event.preventDefault();
+  // sprawdzanie czy kliknięcie jest na img
+  //   if (event.target.nodeName !== "IMG") return;
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "top",
+    captionDelay: 250,
+  });
+  console.log(event.target.alt);
+}
