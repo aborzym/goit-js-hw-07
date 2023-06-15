0d126dd681c50df3ef1c4229c4e0c9e9dae63f0c
aborzym <borzym.a@gmail.com>
2023-06-15 12:04:06 +0200

    basicLB  done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+"use strict";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const imagesArray = galleryItems.map(
+  (img) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="#">
+            <img
+                class="gallery__image"
+                src="${img.preview}" 
+                data-source="${img.original}" 
+                alt="${img.description}"
+            />
+        </a>
+     </li>`
+);
+
+gallery.insertAdjacentHTML("beforeend", imagesArray.join(""));
+
+gallery.addEventListener("click", showBigImage);
+
+function showBigImage(event) {
+  //zablokować domyślne zachowanie
+  event.preventDefault();
+  // sprawdzanie czy kliknięcie jest na img
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`,
+    {
+      onShow: () => {
+        // console.log(`showed ${event.target.alt}`);
+        document.addEventListener("keydown", listenEscapeKey);
+      },
+      onClose: () => {
+        // console.log("closed");
+        document.removeEventListener("keydown", listenEscapeKey);
+      },
+    }
+  );
+  instance.show();
+
+  function listenEscapeKey(event) {
+    if (event.code === "Escape") instance.close();
+  }
+}
