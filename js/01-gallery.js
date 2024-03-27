deb803d0be771c2c55fa89edd75fa45eea68d96e
aborzym <borzym.a@gmail.com>
2024-03-27 16:39:12 +0100

    beginning

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryList = document.querySelector("ul.gallery");
+
+const galleryItemsFragment = document.createDocumentFragment();
+console.log(galleryItemsFragment);
