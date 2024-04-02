90833e95adbc830f2dbf7751b99381c333955a7e
Andrzej Borzym <borzym.a@gmail.com>
2024-04-02 04:14:13 +0200

    task 02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
+
+const createGalleryItem = (image) => {
+  return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"> <img class="gallery__image" src="${image.preview}" alt="${image.description}" /></a></li>`;
+};
+
+const galleryString = galleryItems.map(createGalleryItem).join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryString);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
