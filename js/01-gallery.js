987cc34d46568801488d617ab7790ee56d87d6a0
Andrzej Borzym <borzym.a@gmail.com>
2024-04-02 03:47:22 +0200

    task 01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,29 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-// console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
 
-const galleryList = document.querySelector("ul.gallery");
+//stworzyc funkcję zwracającą pojedynczy obraz w galerii
 
-const galleryItemsFragment = document.createDocumentFragment();
-console.log(galleryItemsFragment);
+const createGalleryItem = (image) => {
+  return `<li class="gallery__item"><a class="gallery__link" href=""> <img class="gallery__image" src="${image.preview}" alt="${image.description}" data-source="${image.original}" /></a></li>`;
+};
+
+const galleryString = galleryItems.map(createGalleryItem).join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryString);
+
+gallery.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  const bigImage = ev.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${bigImage}">`);
+  instance.show();
+
+  //zamknięcie na escape
+  const closeLightboxOnEscape = (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", closeLightboxOnEscape);
+    }
+  };
+  document.addEventListener("keydown", closeLightboxOnEscape);
+});
